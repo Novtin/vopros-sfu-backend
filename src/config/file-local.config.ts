e6e6d8900bb25bf67c7ea4bc8bda04bb5aaa0734
Danil <danil_ustinov@mail.ru>
2025-03-23 11:50:17 +0700

    fix: fix bug

--- src/config/file-local.config.ts
+++ src/config/file-local.config.ts
@@ -4,4 +4,5 @@ import * as process from 'process';
 export default registerAs('fileLocal', () => ({
   storagePath: process.env.FILE_SAVE_PATH,
   storageExamplePath: process.env.FILE_EXAMPLES_SAVE_PATH,
+  maxSize: process.env.FILE_MAX_SIZE,
 }));
--- src/config/multer-image.config.ts
+++ src/config/multer-image.config.ts
@@ -5,6 +5,9 @@ import * as process from 'process';
 import { UnsupportedMediaTypeException } from '@nestjs/common';
 import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
 import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const getMulterImageOptions = (): MulterOptions => {
   return {
